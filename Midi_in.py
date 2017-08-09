0718696aa1b8b4ea019e949663f43a8f072ead57
efrensg96 <efren.sg96@gmail.com>
2017-08-09 14:51:59 +0200

    Update Midi_in.py

--- Midi_in.py
+++ Midi_in.py
@@ -209,10 +209,12 @@ if __name__ == "__main__":
     Sixth_major_octaveUP = Sixth_major + 12
     Seventh_major_octaveUP = Seventh_major + 12
     
+    tempoMessage = mido.Message('clock', time=120)
+    print tempoMessage
      
     for naghama in inport:
         
-        
+        outport.send(tempoMessage)
         
         if naghama.note == Stop_loop.note: # Note C6 (72) closes the code.
             break
