37dd0f48184caed9ec99742f40739735c0017e60
efrensg96 <efren.sg96@gmail.com>
2017-08-10 15:41:06 +0200

    Update Midi_in.py

--- Midi_in.py
+++ Midi_in.py
@@ -231,29 +231,27 @@ if __name__ == "__main__":
     print "clock_interval: ", clock_interval
     print "bpm2tempo: ", mido.bpm2tempo(120)
         
-    tempoMessage = mido.Message('clock')#, time=clock_interval)        
+    clock_interval = 60 / (150 * 24)
+    tempoMessage = mido.Message('clock')#, time=clock_interval)
+    #mido.bpm2tempo(120)
     print tempoMessage
-
     print "Send message 1"
     outport.send(tempoMessage)
-    time.sleep(.5)
-        
+    time.sleep(.017)
     print "Send message 2"
     outport.send(tempoMessage)
-    time.sleep(.5)
-
+    time.sleep(.017)
     print "Send message 3"
     outport.send(tempoMessage)
-    time.sleep(.5)
-        
+    time.sleep(.017)
     print "Send message 4"
     outport.send(tempoMessage)
-    time.sleep(.5)
+    time.sleep(.017)
         
-    print "Send further messages"    
     while True:
+        print "Send now further messages"
         outport.send(tempoMessage)
-        time.sleep(.5)
+        time.sleep(clock_interval)
         
      
     for naghama in inport:
