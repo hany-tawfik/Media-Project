e3fcb70830f3d355472fdb557817b787dd55abdd
efrensg96 <efren.sg96@gmail.com>
2017-08-14 14:23:09 +0200

    Create midi_in_out_ver02.py

---
+++ midi_in_out_ver02.py
@@ -0,0 +1,68 @@
+import mido
+import threading
+import midi_chords as miChords
+
+
+def setup_chords(note_set):
+
+    if Stop_loop.note != note_set:
+        return True
+    else:
+        return False
+
+def sendTempo():
+
+    outport.send(tempoMessage)
+    t = threading.Timer(clock_interval, sendTempo)
+    t.start()
+
+if __name__ == "__main__":
+
+    inputs = mido.get_input_names()
+    outputs = mido.get_output_names()
+
+    inport = mido.open_input('MIDISTART MUSIC 25:MIDISTART MUSIC 25 MIDI 1 24:0')
+    inport2 = mido.open_input('CH345:CH345 MIDI 1 20:0')
+    outport = mido.open_output('CH345:CH345 MIDI 1 20:0')
+
+    Stop_loop = mido.Message('note_on', note=72) # Maybe this is the reason why always it receives 72 when booting
+
+    note = inport.receive()
+
+    Tonic = note.copy()
+    
+    print "Please press a key for choosing a scale"
+
+    while True:
+        if setup_chords(Tonic.note):
+            break
+        note = inport.receive()
+        Tonic = note.copy()
+
+    miChords.Set_Tonic_Scale(Tonic.note)
+    miChords.update_chords()
+
+    STOP_NOTE = 72
+    BPM = 120
+    OFFSET = 7
+    clock_interval = 60. / ((BPM + OFFSET) * 24) #verify without multiplying by 24
+    tempoMessage = mido.Message('clock')  # , time=clock_interval)
+
+    t = threading.Timer(clock_interval, sendTempo)
+    t.start()
+
+    for tone in inport:
+
+        # print "tone.note: ", tone.note
+        # print "Tonic.note: ", Tonic.note
+
+        if tone.note == Stop_loop.note: #Note C6 (72) closes the code.
+            inport.close()
+            inport2.close()
+            outport.close()
+            t.cancel()
+            print "closing program"
+            break
+        else:            
+            miChords.Send_Chord(tone)
+
