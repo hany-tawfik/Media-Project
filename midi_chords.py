e9f26152e216934df83c80db47a96b5e7c43b907
Hany Tawfik <hany-tawfik@hotmail.com>
2017-08-15 15:29:20 +0200

    Update midi_chords.py

--- midi_chords.py
+++ midi_chords.py
@@ -9,9 +9,9 @@ outputs = mido.get_output_names()
 #inport2 = mido.open_input('CH345:CH345 MIDI 1 20:0')
 #outport = mido.open_output('CH345:CH345 MIDI 1 20:0')
 
-inport = mido.open_ioport('MIDISTART MUSIC 25:MIDISTART MUSIC 25 MIDI 1 28:0')
+inport = mido.open_ioport(inputs[0])
 #inport2 = mido.open_input('CH345:CH345 MIDI 1 24:0')
-outport = mido.open_ioport('CH345:CH345 MIDI 1 24:0')
+outport = mido.open_ioport(inputs[1])
 
 
 '''SETTING NOTES/CHORDS'''
