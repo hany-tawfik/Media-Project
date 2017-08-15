27c67ee3738f41e8e6875b8b737849aab345ca73
Hany Tawfik <hany-tawfik@hotmail.com>
2017-08-15 15:46:39 +0200

    Update midi_in_out_ver02.py

--- midi_in_out_ver02.py
+++ midi_in_out_ver02.py
@@ -62,14 +62,14 @@ if __name__ == "__main__":
     #inport2 = mido.open_input('CH345:CH345 MIDI 1 20:0')
     #outport = mido.open_output('CH345:CH345 MIDI 1 20:0')
     x = inputs[0].encode('ascii')
-    print x.dtype
+    y = inputs[1].encode('ascii')
+    print x
+    print y
+
     
-    print 'inputs 0' , inputs[0].encode('ascii')
-    print 'inputs 1', inputs[1].encode('ascii')
-    
-    inport = mido.open_ioport(inputs[0].encode('ascii'))
+    inport = mido.open_ioport(x)
     #inport2 = mido.open_input('CH345:CH345 MIDI 1 24:0')
-    outport = mido.open_ioport(inputs[1].encode('ascii'))
+    outport = mido.open_ioport(y)
 
     Stop_loop = mido.Message('note_on', note=72) # Maybe this is the reason why always it receives 72 when booting
     
