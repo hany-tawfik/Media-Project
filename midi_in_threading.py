318a906db850a0c2071cf846aeac66ea8ccdada1
Hany Tawfik <hany-tawfik@hotmail.com>
2017-08-16 16:07:53 +0200

    Update midi_in_threading.py

--- midi_in_threading.py
+++ midi_in_threading.py
@@ -7,9 +7,6 @@ class SendingNotes(threading.Thread):
     def __init__(self):
         super(SendingNotes, self).__init__()
         self._stop_event = threading.Event()
-        
-    def run(self):
-    
         inputs = mido.get_input_names()
         
         korg = inputs[0].encode('ascii')
@@ -18,6 +15,10 @@ class SendingNotes(threading.Thread):
         inport = mido.open_input(midi_start25)
         inport2 = mido.open_input(korg)
         outport = mido.open_output(korg)
+        
+    def run(self):
+    
+        
         for msg in inport:
             outport.send(msg)
             print msg.note
@@ -36,7 +37,7 @@ while True:
     print "It is working :) "
     time.sleep(1)
     counter +=1
-    if counter >= 10:
+    if counter >= 5:
         x.stop()
         print "closing program"
         break
