b1b4da4471360544bc46164bc92a33bb28aa5358
Hany Tawfik <hany-tawfik@hotmail.com>
2017-08-16 16:38:20 +0200

    Create threading_test.py

---
+++ threading_test.py
@@ -0,0 +1,10 @@
+def non_daemon():
+    time.sleep(5)
+    print 'Test non-daemon'
+
+t = threading.Thread(name='non-daemon', target=non_daemon)
+
+t.start()
+print 'Test one'
+t.join()
+print 'Test two'
