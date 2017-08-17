e8bbdf8e316c353cf6c138ff3592459ecc798d72
efrensg96 <efren.sg96@gmail.com>
2017-08-17 10:16:53 +0200

    Update midi_in_out_ver03.py

--- midi_in_out_ver03.py
+++ midi_in_out_ver03.py
@@ -20,30 +20,9 @@ def sendTempo():
 
         t = threading.Timer(clock_interval, sendTempo)
         t.start()
+        print "sending tempo"
     else:
         print "stop timer"
-
-
-'''        
-def sendTempo():
-
-    global timer_counter
-    outport.send(tempoMessage)
-
-    if stop_timer == False:
-
-        if timer_counter <= 30:
-
-            t = threading.Timer(clock_interval, sendTempo)
-            t.start()
-            timer_counter += 1
-            print "timer_counter: ", timer_counter
-
-    else:
-        print "stop timer"
-
-'''
-
 
 def stop_thread_timer():
 
