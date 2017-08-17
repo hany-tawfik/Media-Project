82a064a2d5ad9d56941bab3f3aae3b41048582ba
efrensg96 <efren.sg96@gmail.com>
2017-08-17 12:00:20 +0200

    Update midi_in_out_ver03.py

--- midi_in_out_ver03.py
+++ midi_in_out_ver03.py
@@ -54,32 +54,22 @@ def test_message():
             miChords.Send_Chord(msg)
 
 
-
-
 if __name__ == "__main__":
 
     inputs = mido.get_input_names()
-    outputs = mido.get_output_names()
-
-    # inport = mido.open_input('MIDISTART MUSIC 25:MIDISTART MUSIC 25 MIDI 1 24:0')
-    # inport2 = mido.open_input('CH345:CH345 MIDI 1 20:0')
-    # outport = mido.open_output('CH345:CH345 MIDI 1 20:0')
 
     korg = inputs[0].encode('ascii')
     midi_start25 = inputs[1].encode('ascii')
 
     inport = mido.open_input(midi_start25)
-    # inport = mido.open_input(midi_start25, callback=test_message)
     inport2 = mido.open_input(korg)
     outport = mido.open_output(korg)
 
-    # inport.callback = test_message
-
     Stop_loop = mido.Message('note_on', note=72) # Maybe this is the reason why always it receives 72 when booting
 
-    note = inport.receive()
+#     note = inport.receive()
 
-    Tonic = note.copy()
+#     Tonic = note.copy()
 
     print "Please press a key for choosing a scale"
 
@@ -103,40 +93,6 @@ if __name__ == "__main__":
 
     midi_thread = threading.Thread(target=test_message)
 
-    # for tone in inport:
-    #
-    #     # print "tone.note: ", tone.note
-    #     # print "Tonic.note: ", Tonic.note
-    #
-    #     if tone.note == Stop_loop.note: #Note C6 (72) closes the code.
-    #         stop_thread_timer()
-    #         inport.close()
-    #         inport2.close()
-    #         outport.close()
-    #         t.cancel()
-    #         t.finished
-    #         print "closing program"
-    #         break
-    #     else:
-    #         miChords.Send_Chord(tone)
-
-    flag = False
-
     midi_thread.start()
     t.start()
     
-    '''           
-    while True:
-
-        #if flag == False:
-         #   print "something"
-           # midi_thread.start()
-           # t.start()
-          #  flag = True
-
-        if stop_timer == True:
-            t.join()
-            midi_thread.join()
-            print "closing program"
-            break
-    '''
