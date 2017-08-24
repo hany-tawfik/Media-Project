8f834494efa64f06d2c360920c6429fb1d8a1433
Hany Tawfik <hany-tawfik@hotmail.com>
2017-08-24 14:11:48 +0200

    Update choosing_tempo_value.py

--- choosing_tempo_value.py
+++ choosing_tempo_value.py
@@ -1,1 +1,10 @@
-import time
+
+
+tempo = [100]
+saved_tempo= [111,109,108,111,100]
+ave_saved_tempo = sum(saved_tempo) / len(saved_tempo)
+
+if  ave_saved_tempo -2 <= tempo[0] <= ave_saved_tempo +2:  
+    saved_tempo.append(tempo[0])
+    clock_interval = update_tempo(ave_saved_tempo)
+    
