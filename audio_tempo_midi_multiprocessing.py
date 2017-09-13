a50c1cdc73576d6338d7cced7acd29849673db7e
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-13 15:52:30 +0200

    Update audio_tempo_midi_multiprocessing.py

--- audio_tempo_midi_multiprocessing.py
+++ audio_tempo_midi_multiprocessing.py
@@ -31,7 +31,7 @@ def stop_all_threads():
 
     global stop_key
     stop_key = True
-    #stop_key_flag.put(stop_key)
+    Stop_key_flag.put(stop_key)
 
 
 def callback_audio(in_data, frame_count, time_info, status):
@@ -69,7 +69,7 @@ def tempo_detection_thread():
     # print "Time needed for Onset and PeakPeaking Calculation:", t1 - t0
 '''
 
-def send_clock_process(clock_interval, clock_value):
+def send_clock_process(clock_interval, clock_value, Stop_key_flag):
     
     interval = clock_interval
     while True:
@@ -78,8 +78,9 @@ def send_clock_process(clock_interval, clock_value):
             interval = clock_value.get()
         outport.send(tempoMessage)
         time.sleep(interval)
-        #if stop_key_flag.get() is True:
-            #break
+        if Stop_key_flag.get() is True:
+            print Stop_key_flag.get()
+            break
 
 def send_tempo_thread():
 
@@ -169,7 +170,8 @@ if __name__ == "__main__":
     
     '''MULTIPROCESS SHARED MEMORIES'''
     clock_value = multiprocessing.Queue()
-    #Stop_key_flag = multiprocessing.Queue()
+    Stop_key_flag = multiprocessing.Queue()
+    Stop_key_flag.put(False)
     #clock_value.put(clock_interval)
     
     while True:
@@ -186,7 +188,7 @@ if __name__ == "__main__":
     midi_thread.start()
     stream.start_stream()
     
-    ext_clock = multiprocessing.Process(target=send_clock_process, args=(clock_interval, clock_value))
+    ext_clock = multiprocessing.Process(target=send_clock_process, args=(clock_interval, clock_value, Stop_key_flag))
     ext_clock.start()
     ext_clock.join()
     '''
