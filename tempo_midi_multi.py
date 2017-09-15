fd0be2bf2273f81837f65a8f5fd51701d40ed397
efrensg96 <efren.sg96@gmail.com>
2017-09-15 15:17:40 +0200

    Update tempo_midi_multi.py

--- tempo_midi_multi.py
+++ tempo_midi_multi.py
@@ -21,7 +21,7 @@ def setup_chords(note_set):
 def update_tempo(new_tempo):
     offset = get_offset_tempo(new_tempo)
     new_clock = 60. / ((new_tempo + offset) * PPQ)
-    # new_clock = np.float16(new_clock)
+    new_clock = np.float16(new_clock)
     return new_clock
 
 
