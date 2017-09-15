13ac3696c947b2d37f8f3725114de980badb5350
efrensg96 <efren.sg96@gmail.com>
2017-09-15 11:24:32 +0200

    Update Media_Project.py

--- Media_Project.py
+++ Media_Project.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     p = pyaudio.PyAudio()
 
     '''AUDIO VARIABLES DEFINITION'''
-    SECONDS = 8
+    SECONDS = 4
     RATE = 44100
     CHUNK = np.uint32(RATE * SECONDS)
     FORMAT = pyaudio.paInt16
