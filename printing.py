1ae7a4ad267de96fd1fa8c4d294c547a0c4e57eb
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-22 15:55:06 +0200

    Update printing.py

--- printing.py
+++ printing.py
@@ -1,1 +1,14 @@
+import mido 
+
+'''MIDI INPUT PORTS SETUP'''
+inputs = mido.get_input_names()
+midi_start25 = inputs[0].encode('ascii')
+inport = mido.open_input(midi_start25)
+Stop_loop = mido.Message('note_on', note=49)
+stop_flag = True
+msg = inport.receive()
+
+for msg in inport:
+  print msg
+
 print "Hi there"
