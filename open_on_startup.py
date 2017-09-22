7004ab71e0f0c057df48d9e9dcab7a5dbd724d96
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-22 13:21:04 +0200

    Update open_on_startup.py

--- open_on_startup.py
+++ open_on_startup.py
@@ -4,7 +4,7 @@ import time
 
 
 def shutdown():
-    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    command = "/usr/bin/sudo /sbin/shutdown -h now"
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
@@ -12,13 +12,13 @@ def shutdown():
  
 print "Pi will shutdown, you have 5 seconds to  ctrl+c"
 time.sleep(5)
-shutdown()
+#shutdown()
     
 #while True:
-execfile("Media_Project.py")
-#execfile("printing.py")
-print " That was open on startup file"
-time.sleep(5)
-print "You will be prompted to choose a new scale"
+    execfile("Media_Project.py")
+    #execfile("printing.py")
+    print " That was open on startup file"
+    time.sleep(5)
+    print "You will be prompted to choose a new scale"
 
 
