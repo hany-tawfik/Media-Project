ede5e2780587c3da6e7de60ad5e4a48724b23780
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-22 15:55:41 +0200

    Update printing.py

--- printing.py
+++ printing.py
@@ -6,7 +6,7 @@ midi_start25 = inputs[0].encode('ascii')
 inport = mido.open_input(midi_start25)
 Stop_loop = mido.Message('note_on', note=49)
 stop_flag = True
-msg = inport.receive()
+#msg = inport.receive()
 
 for msg in inport:
   print msg
