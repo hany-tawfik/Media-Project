e31606799bc841e01cb00d697a3035410e9a5e30
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-22 14:33:39 +0200

    Update open_on_startup.py

--- open_on_startup.py
+++ open_on_startup.py
@@ -1,4 +1,11 @@
 import time
+import mido
+
+
+def shutdownbutton():
+    for msg in inport:
+        if msg.note == Stop_loop.note:
+            shutdown()
 
 
 def shutdown():
@@ -8,9 +15,17 @@ def shutdown():
     output = process.communicate()[0]
     print output
  
+inputs = mido.get_input_names()
+midi_start25 = inputs[1].encode('ascii')
+inport = mido.open_input(midi_start25)
+Stop_loop = mido.Message('note_on', note=49)
+
+midi_thread = threading.Thread(target=shutdownbutton)
+#midi_thread.start()
     
 #while True:
 for i in range(3):
+    
     execfile("Media_Project.py")
     #execfile("printing.py")
     print " That was open on startup file"
