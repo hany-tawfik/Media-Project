3cf1b21651f9afa59144204354914eb530fdebdf
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-22 13:15:37 +0200

    Update open_on_startup.py

--- open_on_startup.py
+++ open_on_startup.py
@@ -2,9 +2,20 @@
 #os.system('printing.py')
 import time
 
+
+def shutdown():
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print output
+    
+    
 #while True:
-  execfile("Media_Project.py")
-  #execfile("printing.py")
-  print " That was open on startup file"
-  time.sleep(5)
-  print "You will be prompted to choose a new scale"
+execfile("Media_Project.py")
+#execfile("printing.py")
+print " That was open on startup file"
+time.sleep(5)
+print "You will be prompted to choose a new scale"
+
+shutdown()
