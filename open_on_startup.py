386e47b6078ac6f59152ed31e770abc06bd0139d
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-22 13:18:08 +0200

    Update open_on_startup.py

--- open_on_startup.py
+++ open_on_startup.py
@@ -9,7 +9,10 @@ def shutdown():
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print output
-    
+ 
+print "Pi will shutdown, you have 5 seconds to  ctrl+c"
+time.sleep(5)
+shutdown()
     
 #while True:
 execfile("Media_Project.py")
@@ -18,4 +21,4 @@ print " That was open on startup file"
 time.sleep(5)
 print "You will be prompted to choose a new scale"
 
-shutdown()
+
