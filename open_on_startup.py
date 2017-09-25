ba3c276e6852ce5acae73c94a7141ab8b592e9ed
Hany Tawfik <hany-tawfik@hotmail.com>
2017-09-25 08:24:46 +0200

    Create open_on_startup.py

---
+++ open_on_startup.py
@@ -0,0 +1,18 @@
+
+
+import time
+
+
+"""MAIN SCRIPT"""
+while stop_flag:
+#for i in range(1):
+    
+    execfile("Media_Project.py")
+    #execfile("printing.py")
+    #print " That was open on startup file"
+    time.sleep(5)
+    #print "You will be prompted to choose a new scale"
+
+print "Shutting doooooooooooooooooown"
+time.sleep(5)
+shutdown()
