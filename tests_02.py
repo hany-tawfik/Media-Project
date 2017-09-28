7c20504d862e4c35c267a8d78b26911b34ad30aa
efrensg96 <efren.sg96@gmail.com>
2017-09-28 18:29:54 +0200

    Update tests_02.py

--- tests_02.py
+++ tests_02.py
@@ -235,7 +235,7 @@ if __name__ == '__main__':
     cv2.destroyAllWindows()
 
     FONT_SIZE = 16
-    string_folder = "05_magnificent_seven_116"
+    string_folder = "06_papillon_126"
     os.chdir('/home/pi/Desktop/Plots')
     os.mkdir(string_folder)
 
